e047500b466587c9f7e3d1bc598b53b22cad2878
Dmitry <dmusolin@inbox.ru>
2024-03-18 00:29:08 +0300

    add pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,11 @@
     <div class="nav-links">
         {#each nav as link}
 
-        <a href={link.href} class="link">{link.title}</a>
+        {#if link.isShow}
+
+<a href={link.href} class="link">{link.title}</a>
+
+{/if}
         
         {/each}
     </div>
@@ -24,23 +28,28 @@
         const nav = [
             {
         title: 'Обо мне',
-        href:'/about'
+        href:'/about',
+        isShow: true
             },
             {
         title: 'Блог',
-        href: '/blog'
+        href: '/blog',
+        isShow: true
             },
             {
         title: 'Портфолио',
-        href: '/project'
+        href: '/project',
+        isShow: true
             },
         {
         title: 'Контакты',
-        href:'/contact'
+        href:'/contact',
+        isShow: true
         },
         {
         title: 'temp',
-        href:'/temp'
-        }
-    ]
+        href:'/temp',
+        isShow: false
+        },
+    ];
         </script>
